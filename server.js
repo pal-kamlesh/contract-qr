4ad8ff2d8d69f20ef1454dd0915eeb20490700b6
pal-kamlesh <kamleshiit5@gmail.com>
2025-02-07 11:40:42 +0530

    done changes

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const port = process.env.PORT || 6000;
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     app.listen(port, console.log(`server is listing on port ${port}`));
   } catch (error) {
     console.log(error);
